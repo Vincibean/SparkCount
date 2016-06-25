300b7dbffdcbc59efcc1ad39e2902ed9e017a7bf
Vincibean <andrebessi00@gmail.com>
2016-06-25 03:22:18 +0200

    Added documentation

--- src/main/scala-2.11/org/vincibean/spark/WordCount.scala
+++ src/main/scala-2.11/org/vincibean/spark/WordCount.scala
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016  Vincibean <Andre Bessi>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.vincibean.spark
 
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
@@ -5,28 +22,31 @@ import org.apache.spark.sql.{Row, SQLContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
+  * Apache Spark main and only job. To be used locally.
+  * Load a text file, convert it into a DataFrame, then count the number of words.
+  *
   * Created by Vincibean on 24/06/16.
   */
 object WordCount {
 
   def main(args: Array[String]): Unit = {
+    // Create the SQLContext from a new SparkContext, which in turn will take a new SparkConf
     val sqlContext = new SQLContext(new SparkContext(new SparkConf()
-      .setAppName("Apache Spark Word Count Example")
-      .setMaster("local[*]")))
+      .setAppName("Apache Spark Word Count Example")                                  // Set a name to this job
+      .setMaster("local[*]")))                                                        // Run the job locally; use all cores.
 
-    val shakespeareRDD = sqlContext.sparkContext
-      .textFile(getClass.getResource("/all-shakespeare.txt").getPath)
-      .flatMap(_.split("""\w+"""))
-      .map(Row(_))
-
+    val shakespeareRDD = sqlContext.sparkContext                                      // Load the Spark Context
+      .textFile(getClass.getResource("/all-shakespeare.txt").getPath)                 // Load the text file from the resources folder
+      .flatMap(_.split("""\w+"""))                                                    // Use a Regex to split per word
+      .map(Row(_))                                                                    // Create a new Row with each String
+    // Define the Schema of the RDD
     val schema = StructType(Array(StructField("word", StringType)))
-
+    // Create a new DataFrame from the RDD given the information contained in the Schema
     val shakespeareDataFrame = sqlContext.createDataFrame(shakespeareRDD, schema)
-
-    val wordCount = shakespeareDataFrame
-      .count
-
-    println(wordCount)
+    // Perform the actual computation: counting
+    val wordCount = shakespeareDataFrame.count
+    // Print the result
+    println(s"There are $wordCount words contained in the documents")
 
   }
 
